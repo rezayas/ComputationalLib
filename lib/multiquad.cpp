012209e9ef156a41665f79f4edf6ad72646f22a6
Eyal Minsky-Fenick <eyjmfc@gmail.com>
2017-07-28 11:16:25 -0400

    Fixed up multiquad (mostly adding comments).

--- lib/multiquad.cpp
+++ lib/multiquad.cpp
@@ -3,13 +3,27 @@
 
 namespace linreg {
   void Multiquad::addDataPoint(const vec &x, double y, double lambda) {
-    const_cast<PolynomialRegression &>(getQR(x))
-      .updateCoefficients(x, y, lambda);
+    // Basically, for each i,
+    // test if brackets(i) is 0 (in which case it's fine),
+    // or if x(i) < mins(i) or x(i) > mins(i) + steps(i) * brackets(i)
+    // (in which case it's not fine).
+    // If all are fine, then continue.
+    if((!brackets.cast<bool>().array()
+	|| ((x.array() >= mins.array())
+	    && ((x - mins).array() <=
+		(stps.cwiseProduct(brackets.cast<double>())).array())))
+       .all())
+      // This is basically a hack.
+      // For technical reasons, it's really hard to avoid code duplication
+      // without this cast.
+      ((PolynomialRegression &)getQR(x)).updateCoefficients(x, y, lambda);
   }
   // Get by indices
   const PolynomialRegression
   &Multiquad::getQRix(const Eigen::VectorXi &ix) const {
     int posn = 0;
+    // Basically, work backwards.
+    // The formula is posn = ∑_i=0^n-1 ix(i)∏_j=i+1^n-1 brackets(j).
     for(int i = brackets.size() - 1, step = 1; i >= 0; i--) {
       if(brackets(i)) {
 	if(ix(i) < 0) {
@@ -26,23 +40,27 @@ namespace linreg {
 	step *= brackets(i);
       }
     }
-    return(lrs[posn]);
+    return(regs[posn]);
   }
   // Get by coordinates
   const PolynomialRegression
   &Multiquad::getQR(const Eigen::VectorXd &x) const {
     try {
-      return(getQRix(((x - lows).array() / stps.array()).cast<int>()));
+      // The method here is to take (x-lows)/stps
+      // (interpreted coefficientwise), and then take the floors.
+      return(getQRix(((x - mins).array() / stps.array()).cast<int>()));
     } catch(const std::out_of_range &e) {
+      // If that throws an exception,
+      // we translate it to what the actual problem was.
       int i;
       for(i = 0; !brackets(i) ||
-	    (x(i) > lows(i) && x(i) < stps(i) * brackets(i) + lows(i)); i++);
+	    (x(i) > mins(i) && x(i) < stps(i) * brackets(i) + mins(i)); i++);
       std::ostringstream msg;
       msg << "Coordinate " << i << "(value: " << x(i) << ") is too ";
-      if(x(i) < lows(i))
-	msg << "low (minimum: " << lows(i);
+      if(x(i) < mins(i))
+	msg << "low (minimum: " << mins(i);
       else
-	msg << "high (maximum: " << stps(i) * brackets(i) + lows(i);
+	msg << "high (maximum: " << stps(i) * brackets(i) + mins(i);
       msg << ')';
       throw std::out_of_range(msg.str());
     }
