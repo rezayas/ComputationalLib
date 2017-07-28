00c78348a95f94afcd0279d7ff5f939cc5cb830a
Eyal Minsky-Fenick <eyjmfc@gmail.com>
2017-07-28 11:59:14 -0400

    Added comments.

--- lib/poly.hpp
+++ lib/poly.hpp
@@ -9,17 +9,34 @@ namespace complib {
       return(exps);
     }
     Eigen::VectorXd coefficients;
+    // Returns the zero polynomial with space to be any polynomial of
+    // the proper order on that many variables.
     Polynomial(int order, int nvars);
+    // Let m be the number of rows in exs and coes.
+    // Let n be the number of columns in exs.
+    // Then the polynomial represented here is:
+    // ∑_i=1^m coes(i)*∏_j=1^n x_j^exs(i,j).
+    // For example, if we want a + bx + cx^2,
+    // then setting exs to [0, 1, 2]ᵀ and coes to [a, b, c]ᵀ
+    // works quite well.
+    // Permuting the rows of both does not change the polynomial.
     inline Polynomial(const Eigen::MatrixXi &exs, const Eigen::VectorXd &coes)
       : exps(exs), coefficients(coes), variables(exs.cols()),
 	monomials(exs.rows()) {}
     double evaluate(const Eigen::VectorXd &) const;
     // Returns the products in the proper order.
+    // Ignores the coefficients.
     Eigen::VectorXd expand(const Eigen::VectorXd &) const;
     void fitToData(const Eigen::MatrixXd &, const Eigen::VectorXd &,
 		   double = 1, double = 0);
     // Returns (g, v, c) such that f(x) = xᵀgx/2 + xᵀv + c,
     // plus possibly terms that are third-order or higher in x.
+    // In other words,
+    // we express f(x) as ∑_i=0^n f_i(x) where f_i is homogenous of degree i
+    // (g is homogenous of degree n if g(λx) = λⁿg(x) for any x),
+    // and return a matrix G such that f_2(x) = xᵀGx/2,
+    // a vector v such that f_1(x) = vᵀx,
+    // and a scalar c such that f_0(x) = c.
     std::tuple<Eigen::MatrixXd, Eigen::VectorXd, double> quaddec() const;
     const int variables, monomials;
   private:
