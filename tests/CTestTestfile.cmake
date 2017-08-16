18d268f0b2c5b5772d83924424201f3ecf4a42b6
Nathan Lin <nathanlukelin@gmail.com>
2017-08-16 16:59:09 -0400

    Removed a bunch of cmake files I accidentally added to commit

--- tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nlin/Documents/LEPH/ComputationalLib/tests
-# Build directory: /Users/nlin/Documents/LEPH/ComputationalLib/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(MyTest "Test")
-set_tests_properties(MyTest PROPERTIES  WORKING_DIRECTORY "</Users/nlin/Documents/LEPH/ComputationalLib/tests>")
